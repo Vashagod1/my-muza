ee6c0162cb99ffb299065c2ce3922108a3a42332
Daniel Izteleuev <dizteleuev@gmail.com>
2025-03-19 15:44:48 +0300

    UPD comlete with musiclist, optimized the music list, making the list only through scss, not through javascript, changed the size of the list.

--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -1,27 +1,17 @@
 import '../styles/components/_musiclist.scss'
 import LikeDislikeButtons from "./UI/LikeDislikeButtons";
 
-export default function MusicList({playlist, title, author, cover}) {
+export default function MusicList({title, author, cover}) {
 
     return (
-        <>
-            <div className="playlists">
-                <div className="playlists__item">
-                    <div className="playlists__item__track">
-
-                    </div>
-                    <img
-                        src={cover}
-                        alt={`Обложка альбома: ${title}`}
-                        className="playlists__item__cover"
-                    />
-                    <div className="playlists__item__description">
-                        <h3 className="playlists__item__description__title">{title}</h3>
-                        <h4 className="playlists__item__description__subtitle">{author}</h4>
-                    </div>
-                    <LikeDislikeButtons playlist={playlist}/>
-                </div>
+        <div className="playlists__item">
+            <img src={cover} alt={`Обложка альбома: ${title}`} className="playlists__item__cover"/>
+            <div className="playlists__item__description">
+                <h3 className="playlists__item__description__title">{title}</h3>
+                <h4 className="playlists__item__description__subtitle">{author}</h4>
             </div>
-        </>
-    )
+            <LikeDislikeButtons />
+        </div>
+    );
 }
+
--- src/components/MusicListSection.jsx
+++ src/components/MusicListSection.jsx
@@ -6,19 +6,21 @@ const playlists = [
         num: '1',
         title: 'Golden',
         author: 'NeonNiteClub',
-        length: '3:27',
+        length: '3:28',
         cover: '/images/NeonNiteClub.jpg'
     },
     {
         id: 2,
         title: 'Back Up Friend',
         author: 'FromTom',
+        length: '2:44',
         cover: '/images/BackUpFriend.jpg',
     },
     {
         id: 3,
         title: 'Compañeros',
         author: 'Sundaypanic',
+        length: '1:44',
         cover: '/images/Compañeros.jpg',
     },
     {
@@ -26,27 +28,55 @@ const playlists = [
         title: 'The King',
         author: 'Cullah',
         cover: '/images/TheKing.jpg',
-    }
+    },
+    {
+        id: 5,
+        title: 'Never let go',
+        author: 'NDA',
+        cover: '/images/NDA.jpg',
+    },
+    {
+        id: 6,
+        title: 'More Than Friends',
+        author: 'pyzow',
+        cover: '/images/MoreThanFriends.jpeg',
+    },
+    {
+        id: 7,
+        title: 'Could I Be Enough? ',
+        author: 'Roads',
+        cover: '/images/CouldIBeEnough.jpg',
+    },
+    {
+        id: 8,
+        title: 'Sorrows Away',
+        author: 'MarcusWay',
+        cover: '/images/SorrowAway.jpg',
+    },
+    {
+        id: 9,
+        title: 'Voices',
+        author: 'PNFA',
+        cover: '/images/Voices.jpg',
+    },
+    {
+        id: 10,
+        title: 'outttt',
+        author: 'AnasAmin',
+        cover: '/images/outttt.jpeg',
+    },
 ];
 
 
 export default function MusicListSection() {
-
     return (
         <div className="music-list-section">
-            <div className="music-list-section__content">
-                <h2 className="playlists__title">Новинки этого года</h2>
+            <h2 className="playlists__title">Новинки этого года</h2>
+            <div className="playlists__columns">
                 {playlists.map(playlist => (
-                    <MusicList key={playlist.id}
-                               playlist={playlist}
-                               num={playlist.num}
-                               title={playlist.title}
-                               author={playlist.author}
-                               length={playlist.length}
-                               cover={playlist.cover}
-                    />
+                    <MusicList key={playlist.id} {...playlist} />
                 ))}
             </div>
         </div>
-    )
+    );
 }
